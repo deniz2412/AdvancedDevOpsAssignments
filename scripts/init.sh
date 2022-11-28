378ecf4606769f4a3900e7ffdf5aa1139788b03c
Deniz Hadžirušidović <denizh@comtrade.com>
2022-11-28 22:16:20 +0100

    V4

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 nohup ./scripts/cleanup.sh &
-./tomcat/bin/catalina.sh run 
+./bin/catalina.sh run 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ##Script creates 2 folders and pulls tomcat dir and prepares it
-mkdir -vp config
+mkdir -vp conf
 
 curl -L https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.69/bin/apache-tomcat-9.0.69.tar.gz \
      -o tomcat.tar.gz
